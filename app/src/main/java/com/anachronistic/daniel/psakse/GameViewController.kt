b4eac98b5117669ca94f5e9d1f604e25d85e407e
Daniel <danielmarriner@anachronistic-tech.co.uk>
2019-11-30 00:48:46 +0000

    Started migration to new UI layout system

--- app/src/main/java/com/anachronistic/daniel/psakse/GameViewController.kt
+++ app/src/main/java/com/anachronistic/daniel/psakse/GameViewController.kt
@@ -1,72 +1,80 @@
 package com.anachronistic.daniel.psakse
 
-import android.app.Activity
 import android.content.Context
-import android.graphics.Point
 import android.os.Bundle
+import android.support.constraint.ConstraintLayout
+import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.view.ViewTreeObserver
+import android.widget.FrameLayout
 import android.widget.ImageButton
-import android.widget.LinearLayout
 import android.widget.Toast
+//import androidx.core.view.doOnLayout
 
-class GameViewController: Activity() {
+class GameViewController: AppCompatActivity() {
 
-    val gridSize = 5
-    val wildcards = 2
-    var grid:Grid? = null
-    var deck:Deck? = null
-    var activeCard:Card? = null
-    var lastSelected = -1
-    var gameComplete = false
-    var puzzleID: String? = null
-    var override: String? = null
-    var puzzleSig: String = ""
-    var dSize = Point()
+    private val gridSize = 5
+    private val wildcards = 2
+    private var grid: Grid? = null
+    private var deck: Deck? = null
+    private var activeCard: Card? = null
+    private var lastSelected = -1
+    private var gameComplete = false
+    private var puzzleID: String? = null
+    private var override: String? = null
+    private var puzzleSig: String = ""
+
+    // TODO: Add button views
+    private var mainGrid: FrameLayout? = null
+    private var subGrid: FrameLayout? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val display = windowManager.defaultDisplay
-        val size = Point()
-        display.getRealSize(size)
-        this.dSize = size
-        resetGame(this, dSize)
+        setContentView(R.layout.activity_game_view_controller)
 
-//        var linearLayout = LinearLayout(this)
-//        linearLayout.orientation = LinearLayout.VERTICAL
-////
-//        var layoutParams = LinearLayout.LayoutParams(ActionBar.LayoutParams.FILL_PARENT, ActionBar.LayoutParams.WRAP_CONTENT)
-//        this.addContentView(linearLayout, layoutParams)
-        
-//        var button = ImageButton(this)
-//        button.text = "My button"
-//        button.layoutParams = params
-//
-//        linearLayout.addView(button)
-
+        // TODO: Fix this mess
+        val context = this
+        val rootView: ConstraintLayout = findViewById(R.id.root)
+//        rootView.viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
+//            override fun onGlobalLayout() {
+//                mainGrid = findViewById(R.id.mainGrid)
+//                subGrid = findViewById(R.id.subGrid)
+//                resetGame(context)
+//            }
+//        })
+//        rootView.doOnLayout {
+//                mainGrid = findViewById(R.id.mainGrid)
+//                subGrid = findViewById(R.id.subGrid)
+//                resetGame(context)
+//        }
     }
 
-    private fun resetGame(view: Context, display: Point) {
+    override fun onResume() {
+        super.onResume()
+    }
+
+    // TODO: Implement
+    private fun setupButtonView(button: ImageButton, title: String, color: Colors) {}
+
+    private fun resetGame(context: Context) {
         gameComplete = false
         deck = Deck()
         if (grid != null) {
-            for (i in 0 until grid?.buttonGrid!!.size) {
-                grid?.grid!![i] = null
-                grid?.buttonGrid!![i].reset()
-                grid?.buttonGrid!![i].isEnabled = true
-            }
+            // Reset all buttons in main and side grids
+            grid!!.reset()
         } else {
-            grid = Grid(gridSize, display)
-            val params = LinearLayout.LayoutParams(display.x - 60, display.y)
-            val gridView = grid?.create(view)
-            gridView?.layoutParams = params
-            val resetBtn = grid!!.drawControls(this)
-            resetBtn.setOnClickListener(resetHandler())
-            gridView?.addView(resetBtn)
-            this.addContentView(gridView, params)
-            for (button in grid?.buttonGrid!!) {
-                button.reset()
+            // Create main and side grids with all buttons
+            grid = Grid(gridSize, mainGrid!!, subGrid!!, context)
+            for (button in grid!!.buttonGrid) {
                 button.setOnClickListener(select(button))
-                button.isEnabled = true
+            }
+
+            // Create in game controls
+            // TODO: Pass subviews
+            if (puzzleID != null) {
+//                setupButtonView(, "Reset", Colors.Purple)
+            } else {
+//                setupButtonView(, "New Game", Colors.Purple)
             }
         }
 
@@ -80,8 +88,8 @@ class GameViewController: Activity() {
                 locked = locked.drop(4)
                 val card = deck!!.stringToCard(col, sym)
                 val image = card.getFilename()
-                val bgcolor = card.getColor()
-                grid!!.buttonGrid[pos].setAttrs(image, bgcolor, 9, R.color.gameFBorder)
+                val color = card.getColor()
+                grid!!.buttonGrid[pos].setAttrs(image, color, 9, R.color.gameFBorder)
                 grid!!.grid[pos] = card
             }
             deck!!.arr = deck!!.createDeckFromString(override!!.drop(12))
@@ -94,11 +102,11 @@ class GameViewController: Activity() {
             for (i in 0 until 3) {
                 var randPosition = (Math.random() * (gridSize * gridSize)).toInt()
                 while (randArray.contains(randPosition) ||
-                        randPosition == gridSize * gridSize ||
-                        randArray.contains(randPosition - 1) ||
-                        randArray.contains(randPosition + 1) ||
-                        randArray.contains(randPosition - gridSize) ||
-                        randArray.contains(randPosition + gridSize)) {
+                    randPosition == gridSize * gridSize ||
+                    randArray.contains(randPosition - 1) ||
+                    randArray.contains(randPosition + 1) ||
+                    randArray.contains(randPosition - gridSize) ||
+                    randArray.contains(randPosition + gridSize)) {
                     randPosition = (Math.random() * (gridSize * gridSize - 1)).toInt()
                     if (randPosition == 0) {
                         randPosition = 1
@@ -108,8 +116,8 @@ class GameViewController: Activity() {
             }
             for (i in randArray) {
                 val image = deck!!.arr[0].getFilename()
-                val bgcolor = deck!!.arr[0].getColor()
-                grid!!.buttonGrid[i].setAttrs(image, bgcolor, 9, R.color.gameFBorder)
+                val color = deck!!.arr[0].getColor()
+                grid!!.buttonGrid[i].setAttrs(image, color, 9, R.color.gameFBorder)
                 grid!!.buttonGrid[i].isEnabled = false
                 grid!!.grid[i] = deck!!.arr[0]
                 val card = deck!!.arr.removeAt(0)
@@ -122,42 +130,46 @@ class GameViewController: Activity() {
 
         // Shuffle deck and draw tile
         deck?.finalShuffle()
-        val image = deck!!.arr[0].getFilename()
-        val bgcolor = deck!!.arr[0].getColor()
-        grid!!.grid[gridSize * gridSize] = deck!!.arr[0]
-        grid!!.buttonGrid[gridSize * gridSize].setAttrs(image, bgcolor, 0, R.color.gameSBorder)
+        setCard((gridSize * gridSize), deck!!.arr[0])
     }
+
+    // TODO: Implement
+    private fun sendToServer(puzzle: String) {}
 
     private fun select(sender: ImageButton): View.OnClickListener {
         return View.OnClickListener {
             if (activeCard != null) {
+                // if sender == gridSize^2 or sender == lastSelected
+                // deselect
+                // else if sender empty
+                // try move()
+                // else
+                // try swap()
                 if (it.tag == (gridSize * gridSize) || it.tag == lastSelected) {
+                    // If location is card location or deck location deselect
                     deselect()
                 } else {
                     val location = it.tag as Int
                     if (grid!!.grid[location] == null) {
-                        // try move
+                        // If location empty try move
                         if (checker(location, activeCard!!) || location > (gridSize * gridSize)) {
-                            grid!!.grid[location] = activeCard
-                            grid!!.buttonGrid[location].setAttrs(activeCard!!.getFilename(), activeCard!!.getColor(),
-                                0, R.color.gameSBorder)
+                            // If no tile conflicts place card
+                            setCard(location, activeCard)
+                            // clear previous location
                             clearTile(lastSelected)
                         } else {
+                            // Warn of tile move conflict
                             Toast.makeText(this, "That tile can't be placed there", Toast.LENGTH_SHORT).show()
                         }
                         deselect()
-                        val finishedArray = arrayListOf<Boolean>()
                         if (deck!!.arr.size == 0) {
-                            for (i in 0 until (gridSize * (gridSize + 1))) {
-                                if (i < gridSize * gridSize) {
-                                    finishedArray.add(grid!!.grid[i] != null)
-                                } else {
-                                    finishedArray.add(grid!!.grid[i] == null)
-                                }
-                            }
-                            if (!finishedArray.contains(false)) {
+                            // If deck empty check grid full
+                            val arr: List<Boolean> = grid!!.grid.map { a -> a != null }
+                            arr.dropLast(5)
+                            if (arr.contains(false)) {
+                                // If grid full, game is complete
                                 gameComplete = true
-                                // save completion or send to server
+                                // TODO: save completion or send to server
                                 for (i in grid!!.buttonGrid) {
                                     i.isEnabled = false
                                 }
@@ -165,20 +177,16 @@ class GameViewController: Activity() {
                             }
                         }
                     } else {
-                        // try swap
+                        // If location not empty try swap
                         if (lastSelected != gridSize * gridSize) {
-                            if ((checker(lastSelected, grid!!.grid[location]!!) || lastSelected > (gridSize *
-                                        gridSize)) && (checker(location, activeCard!!) ||  location > (gridSize *
-                                        gridSize))) {
-                                grid!!.grid[lastSelected] = grid!!.grid[location]
-                                var image = grid!!.grid[lastSelected]!!.getFilename()
-                                var color = grid!!.grid[lastSelected]!!.getColor()
-                                grid!!.buttonGrid[lastSelected].setAttrs(image, color, 0, R.color.gameSBorder)
-                                grid!!.grid[location] = activeCard
-                                image = grid!!.grid[location]!!.getFilename()
-                                color = grid!!.grid[location]!!.getColor()
-                                grid!!.buttonGrid[location].setAttrs(image, color, 0, R.color.gameSBorder)
+                            // If last selected card was not from the deck
+                            if ((checker(lastSelected, grid!!.grid[location]!!) || lastSelected >
+                                        (gridSize * gridSize)) && (checker(location, activeCard!!) ||  location > (gridSize * gridSize))) {
+                                // If cards won't conflict when swapped, swap cards
+                                setCard(lastSelected, grid!!.grid[location])
+                                setCard(location, activeCard)
                             } else {
+                                // Warn of tile swap conflict
                                 Toast.makeText(this, "Those tiles can't be swapped", Toast.LENGTH_SHORT).show()
                             }
                         }
@@ -211,106 +219,98 @@ class GameViewController: Activity() {
         lastSelected = -1
     }
 
+    private fun setCard(location: Int, card: Card?) {
+        if (card != null) {
+            grid!!.grid[location] = card
+            grid!!.buttonGrid[location].setAttrs(card.getFilename(), card.getColor(), 0, R.color.gameSBorder)
+        } else {
+            grid!!.grid[location] = null
+            grid!!.buttonGrid[location].setAttrs("clear", R.color.gameWhite, 0, R.color.gameSBorder)
+        }
+    }
+
     private fun clearTile(position: Int) {
         if (position == (gridSize * gridSize)) {
             deck!!.arr.removeAt(0)
             if (deck!!.arr.size >= 1) {
-                val image = deck!!.arr[0].getFilename()
-                val color = deck!!.arr[0].getColor()
-                grid!!.grid[gridSize * gridSize] = deck!!.arr[0]
-                grid!!.buttonGrid[gridSize * gridSize].setAttrs(image, color, 0, R.color.gameSBorder)
+                setCard((gridSize * gridSize), deck!!.arr[0])
             } else {
                 grid!!.grid[gridSize * gridSize] = null
                 grid!!.buttonGrid[gridSize * gridSize].setAttrs("empty", R.color.gameWhite, 0, R.color.gameSBorder)
                 grid!!.buttonGrid[gridSize * gridSize].isEnabled = false
             }
         } else {
-            grid!!.grid[position] = null
-            grid!!.buttonGrid[position].setAttrs("clear", R.color.gameWhite, 0, R.color.gameSBorder)
+            setCard(position, null)
         }
     }
 
-    private fun left(x: Int): Int {
-        return x + 1
-    }
-    private fun right(x: Int): Int {
-        return x - 1
-    }
-    private fun up(x: Int): Int {
-        return x + gridSize
-    }
-    private fun down(x: Int): Int {
-        return x - gridSize
-    }
-
     private fun checker(position: Int, card: Card): Boolean {
-        val validArray = arrayListOf<Boolean>()
+        fun checkTile(position: Int, card: Card): Boolean {
+            if (position > (gridSize * gridSize)) { return true }
+            return if (grid!!.grid[position] != null) {
+                card.matches(grid!!.grid[position]!!)
+            } else { true }
+        }
+        fun left(x: Int): Int { return x + 1 }
+        fun right(x: Int): Int { return x - 1 }
+        fun up(x: Int): Int { return x + gridSize }
+        fun down(x: Int): Int { return x - gridSize }
+
         if (position < gridSize) {
             if (position == 0) {
                 // Bottom right corner; check tiles left and above
-                validArray.add(checkTile(left(position), card))
-                validArray.add(checkTile(up(position), card))
+                if (checkTile(left(position), card)) { return false }
+                if (checkTile(up(position), card)) { return false }
             } else if (position == gridSize - 1) {
                 // Bottom left corner; check tiles right and above
-                validArray.add(checkTile(right(position), card))
-                validArray.add(checkTile(up(position), card))
+                if (checkTile(right(position), card)) { return false }
+                if (checkTile(up(position), card)) { return false }
             } else {
                 // Bottom edge; check tiles left, right and above
-                validArray.add(checkTile(left(position), card))
-                validArray.add(checkTile(right(position), card))
-                validArray.add(checkTile(up(position), card))
+                if (checkTile(left(position), card)) { return false }
+                if (checkTile(right(position), card)) { return false }
+                if (checkTile(up(position), card)) { return false }
             }
         } else if (position % gridSize == 0) {
             if (position == gridSize * (gridSize - 1)) {
                 // Top right corner; check tiles left and below
-                validArray.add(checkTile(left(position), card))
-                validArray.add(checkTile(down(position), card))
+                if (checkTile(left(position), card)) { return false }
+                if (checkTile(down(position), card)) { return false }
             } else {
                 // Right edge; check tiles left, above and below
-                validArray.add(checkTile(left(position), card))
-                validArray.add(checkTile(up(position), card))
-                validArray.add(checkTile(down(position), card))
+                if (checkTile(left(position), card)) { return false }
+                if (checkTile(up(position), card)) { return false }
+                if (checkTile(down(position), card)) { return false }
             }
         } else if (position % gridSize == gridSize - 1) {
             if (position == (gridSize * gridSize) - 1) {
                 // Top left corner; check tiles right and below
-                validArray.add(checkTile(right(position), card))
-                validArray.add(checkTile(down(position), card))
+                if (checkTile(right(position), card)) { return false }
+                if (checkTile(down(position), card)) { return false }
             } else {
                 // Left edge; check tiles right, above and below
-                validArray.add(checkTile(right(position), card))
-                validArray.add(checkTile(up(position), card))
-                validArray.add(checkTile(down(position), card))
+                if (checkTile(right(position), card)) { return false }
+                if (checkTile(up(position), card)) { return false }
+                if (checkTile(down(position), card)) { return false }
             }
         } else if (position > gridSize * (gridSize - 1)) {
             // Top edge; check tiles left, right and below
-            validArray.add(checkTile(left(position), card))
-            validArray.add(checkTile(right(position), card))
-            validArray.add(checkTile(down(position), card))
+            if (checkTile(left(position), card)) { return false }
+            if (checkTile(right(position), card)) { return false }
+            if (checkTile(down(position), card)) { return false }
         } else {
             // Central tile; check tiles left, right, above and below
-            validArray.add(checkTile(left(position), card))
-            validArray.add(checkTile(right(position), card))
-            validArray.add(checkTile(up(position), card))
-            validArray.add(checkTile(down(position), card))
+            if (checkTile(left(position), card)) { return false }
+            if (checkTile(right(position), card)) { return false }
+            if (checkTile(up(position), card)) { return false }
+            if (checkTile(down(position), card)) { return false }
         }
-        return !validArray.contains(false)
-    }
-
-    private fun checkTile(position: Int, card: Card): Boolean {
-        if (position > (gridSize * gridSize)) {
-            return true
-        }
-        if (grid!!.grid[position] != null) {
-            return card.matches(grid!!.grid[position]!!)
-        } else {
-            return true
-        }
+        return true
     }
 
     fun resetHandler(): View.OnClickListener {
         return View.OnClickListener {
-            resetGame(this, dSize)
+            resetGame(this)
         }
     }
 }
