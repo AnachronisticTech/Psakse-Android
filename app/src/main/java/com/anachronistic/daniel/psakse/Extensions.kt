dbec964e4e77d556593c55a1cfe6b63a8bd34150
Daniel <danielmarriner@anachronistic-tech.co.uk>
2019-12-01 01:30:28 +0000

    Fixed game completion reset bug

--- app/src/main/java/com/anachronistic/daniel/psakse/Extensions.kt
+++ app/src/main/java/com/anachronistic/daniel/psakse/Extensions.kt
@@ -6,6 +6,7 @@ import android.widget.ImageButton
 
 fun ImageButton.reset() {
     this.setAttrs("clear", R.color.gameWhite, 0, R.color.gameSBorder)
+    this.isEnabled = true
 }
 
 fun ImageButton.setAttrs(image: String, bgColor: Int, sWidth: Int, sColor: Int) {
